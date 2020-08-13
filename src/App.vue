dcec1eb634e1aafd851ca9a7377417ac4af8719e
celfonica <servicioscelfonica@gmail.com>
2020-08-13 15:05:14 -0700

    Adding vue routing, about, login and home views

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,52 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Apesyntax logo"
-          class="shrink mr-2"
-          contain
-          src="https://celfonica.s3-us-west-1.amazonaws.com/logos/apesyntax+logo.png"
-          transition="scale-transition"
-          width="40"
-        />
+<v-app>
+  <!-- navbar -->
+    <div id="nav">
+      <v-app-bar
+        app
+        color="primary"
+        dark
+      >
+        <div class="d-flex align-center">
+          <v-img
+            alt="Apesyntax logo"
+            class="shrink mr-2"
+            contain
+            src="https://celfonica.s3-us-west-1.amazonaws.com/logos/apesyntax+logo.png"
+            transition="scale-transition"
+            width="40"
+          />
 
-        <v-img
-          alt="Apesyntax Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://celfonica.s3-us-west-1.amazonaws.com/logos/Apesyntax+name+white.png"
-          width="200"
-        />
+          <v-img
+            alt="Apesyntax Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://celfonica.s3-us-west-1.amazonaws.com/logos/Apesyntax+name+white.png"
+            width="200"
+          />
+        </div>
+
+        <v-spacer></v-spacer>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+
+
+        </v-app-bar>
       </div>
 
-      <v-spacer></v-spacer>
+     <!-- body -->
+      <div id="body">
+        <main>
+          <router-view/>
+        </main>
+     </div>
 
-    
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <!-- footer -->
+    <div id="footer">
     <v-footer
       dark
       padless
-    >
-      <v-card
         flat
         tile
         class=" white--text text-center"
@@ -48,7 +57,7 @@
           <v-btn
           v-for="icon in icons"
           :key="icon"
-          class="mx-3"
+          class="mx-3 "
           dark
           icon
         >
@@ -61,34 +70,50 @@
         <v-card-text class="white--text">
           {{ new Date().getFullYear() }} — <strong>Apesyntax</strong>
         </v-card-text>
-      </v-card>
+
     </v-footer>
+  </div>
   </v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    icons: [
-      {
-          name:'mdi-google',
-          url: 'https://business.google.com/dashboard/l/18315130497288576188',
-          target: '_blank'
-      },
-      {
-          name:'mdi-github',
-          url: 'https://www.github.com/abraham-felix',
-          target: '_blank'
-      },
-    ]
-  }),
-};
+data: () => ({
+  icons: [
+    {
+        name:'mdi-google',
+        url: 'https://business.google.com/dashboard/l/18315130497288576188',
+        target: '_blank'
+    },
+    {
+        name:'mdi-github',
+        url: 'https://www.github.com/abraham-felix',
+        target: '_blank'
+    },
+  ]
+})
+}
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="about">
+<v-card
+   class="mx-auto"
+   max-width="344"
+ >
+ <v-img
+   alt="Apesyntax logo"
+   class="shrink mr-2 margin center"
+   contain
+   src="https://celfonica.s3-us-west-1.amazonaws.com/logos/apesyntax+logo.png"
+   transition="scale-transition"
+   width="100"
+ />
+  <v-card-text>
+    <h1>Apesyntax</h1>
+    <p> Hello, I am abraham felix i am trying to develop something cool,
+    like a library for us to use in our code and add cool features,
+    so <span class="bold">hang</span> on tight this could be quite a journey!  </p>
+  </v-card-text>
+  </v-card>
+  </div>
+</template>
+
+
+<style scoped>
+  h1 {
+    margin-bottom:1vw;
+  }
+  .bold {
+    -webkit-text-stroke-width: medium;
+  }
+  .v-card{
+    display:inline-block;
+  }
+  .center{
+    margin-left: auto !important;
+    margin-right: auto !important;
+    display: -webkit-inline-box !important;
+  }
+  .margin {
+    margin:10px !important;
+  }
+  .about {
+   min-height: 900px;
+  }
+  .mx-auto {
+   margin-top: 10vw;
+  }
+</style>
