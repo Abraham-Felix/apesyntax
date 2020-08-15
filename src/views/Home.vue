50d9db3a8bb88a40f9d2fde8787eed080bc0e90d
celfonica <servicioscelfonica@gmail.com>
2020-08-14 20:52:32 -0700

    Home initial welcome

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,48 +14,8 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Front end Developing
+          welcome!
         </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with front end application,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
 
 
       </v-col>
@@ -78,3 +38,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .body {
+    heigth:600px;
+  }
+</style>
