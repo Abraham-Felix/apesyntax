83c2f6f283d6f895f97b5b527e4b0be04a199522
celfonica <servicioscelfonica@gmail.com>
2020-08-20 23:01:35 -0700

    Home.vue | syntax clean up | update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,77 +1,65 @@
+<style >
+
+li {
+    color: dodgerblue;
+    font-size: 18px;
+    padding: 1vw;
+}
+
+.v-card {
+    width: 420px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 3vw;
+}
+
+.v-image {
+    width: min-content;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+</style>
+
 <template>
-  <v-container>
+
+<v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-tooltip left >
-          <template v-slot:activator="{ on, attrs }">
-        <v-img
-          :src="require('../assets/apesyntaxlogo.png')"
-          class="my-10"
-          contain
-          v-bind="attrs"
-          v-on="on"
-          height="200"
-        />
-        </template>
-        <img class="monk-ico" src="https://celfonica.s3-us-west-1.amazonaws.com/logos/monk-circle+50px.png">
-
-         <span style="display:inline;">
-           you hovered over me!? didn't you?... <br><br>
-         Hello I am Monk,<br>
-         a virtual Ape here to help you!<br><br>
-      <p>"ignore my weird behaviors"</p><br>
-
-    </span>
-
-
-        </v-tooltip>
-      </v-col>
-      <v-col class="mb-4">
-      <v-card >
-        <h1 class="display-2 font-weight-bold mb-3">
-          Hello, World.
-        </h1>
-        <p>
-         This app runs node.js server and vue.js framework contected to firebase db </p>
-         <h4> - New features coming soon!</h4>
-         <li > Skeleton Loader with real data</li>
-         <li > Lesson form/viewer/editor </li>
-         <li > google auth / git auth  </li>
-         <br><br>
-         <h4>Look around... or send a message to <router-link to="/contact">contact</router-link></h4><br>
-
-        </v-card>
+        <v-col cols="12">
+            <v-tooltip left>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-img :src="require('../assets/apesyntaxlogo.png')" class="my-10" contain v-bind="attrs" v-on="on" height="200" />
+                </template>
+                <img class="monk-ico" src="https://celfonica.s3-us-west-1.amazonaws.com/logos/monk-circle+50px.png">
+                <span style="display:inline;">
+             you hovered over me!? didn't you?... <br><br>
+             Hello I am Monk,<br>
+             a virtual Ape here to help you!<br><br>
+            <p>"ignore my weird behaviors"</p><br>
+            </span>
+            </v-tooltip>
+        </v-col>
+        <v-col class="mb-4">
+            <v-card>
+                <h3> New features coming soon!</h3>
+                <li> Skeleton Loader with real data</li>
+                <li> Lesson form/viewer/editor </li>
+                <li> google auth / git auth </li>
+                <br>
+                <br>
+                <h4>Look around... or send a message to <router-link to="/contact">contact</router-link></h4>
+                <br>
+            </v-card>
         </v-col>
     </v-row>
-  </v-container>
+</v-container>
+
 </template>
 
 <script>
 
-
 export default {
-  name: 'home'
+    name: 'home'
 }
+
 </script>
-
-<style >
-  li {
-  color: dodgerblue;
-  font-size: 18px;
-  padding: 1vw;
-  }
-  .v-card {
-    width: 420px;
-    margin-left:auto;
-    margin-right:auto;
-    padding: 3vw;
-  }
-  .v-image {
-    width:min-content;
-    margin-left:auto;
-    margin-right:auto;
-  }
-  .noti {
-
-  }
-</style>
