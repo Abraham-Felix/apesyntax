e4d18598b5dcb8523000448cb3e64a962e6b3339
celfonica <servicioscelfonica@gmail.com>
2020-09-06 16:49:59 -0700

    Profile | update

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -14,79 +14,122 @@ button {
     height: 50px !important;
     min-width: 50px !important;
 }
-
+.v-container {
+  padding:0px !important;
+}
 .v-card {
-    margin-top: 10vw;
-    margin-bottom: 10vw;
+    margin-top: 1vw;
+    margin-bottom: 1vw;
     height: auto;
-    padding: 2vh;
+    padding: 1vw;
     max-width: 600px !important;
     width: auto;
     -webkit-box-pack: center;
 }
-
+@media only screen and (min-width:1000px){
+  .v-card {
+    width: 100% !important;
+    max-width: 1000px !important;
+  }
+  form {
+      width: 100%;
+      display: table-cell !important;
+      /* height: 117px; */
+      box-shadow: 0px 0px 19px 4px #e6e8ec;
+      border-radius: 10px;
+      padding: 10px;
+  }
+}
+@media ony screen and (max-width:1000px){
+  .right-btn{
+     float: right;
+     position: relative;
+     top: -10px;
+     left: -125px;
+}
+form {
+    padding: 10px;
+    box-shadow: 0px 0px 10px 4px #e6e8ec;
+    margin-top:20px;
+    border-radius: 5px;
+}
+}
+.authicons{
+  margin-left:10px;
+  marginright:10px;
+}
 .block {
     display: block!important;
 }
-
 .uid {
   font-size: 12px;
+}
+.update-profile-inputs{
+display: inline-grid;
+}
+.update{
+  height:28px !important;
+  width:fill-available;
 }
 </style>
 
 <template>
 
 <div id="app">
-    <v-container>
+    <v-container class="v-container">
         <v-card class="center block">
           <v-text-field readonly v-model="uid" label="Uid">
           </v-text-field>
             <img :src="authUser.photoURL" width="150">
             <p>What's up, {{authUser.displayName || 'my friend'}}  I know you like {{authUser.favoriteFood || 'food'}}</p>
              <br>
-             <v-icon color=green v-if="linkedGoogle" >mdi-google</v-icon>
-             <v-icon color=green v-if="linkedPassword"> mdi-email-check</v-icon>
+             <v-icon class="authicons" color=green v-if="linkedGoogle" >mdi-google</v-icon>
+             <v-icon class="authicons" color=green v-if="linkedPassword"> mdi-email-check</v-icon>
              <v-divider class="m-tb-20"></v-divider>
 
             <form  @submit.prevent="updateProfile">
               <h4>Update Profile</h4>
               <br>
-              <div>
+              <div class="update-profile-inputs">
               <input  class="form-control" v-model="displayName" placeholder="your name"><br>
               <input class="form-control" v-model="photoURL" placeholder="your photo url">
               </div>
-              <v-btn depressed small color="primary" @keyup.enter="updateProfile"> update </v-btn>
+              <v-btn depressed small color="primary" @keyup.enter="updateProfile" class="update right-btn"> update </v-btn>
             </form>
 
             <form  @submit.prevent="updateEmail">
               <h4>Update email</h4>
               <br>
               <input type="email" class="form-control" v-model="email" placeholder="enter new email">
-              <v-btn depressed small color="primary" @keyup.enter="updateEmail"> update </v-btn>
+              <v-btn depressed small color="primary" @keyup.enter="updateEmail" class="update"> update </v-btn>
             </form>
 
             <form  @submit.prevent="updatePassword">
               <h4>Update password</h4>
               <br>
               <input type="password" class="form-control" v-model="newPassword" placeholder="enter new password">
-              <v-btn depressed small color="primary" @keyup.enter="updatePassword"> update </v-btn>
+              <v-btn depressed small color="primary" @keyup.enter="updatePassword" class="update"> update </v-btn>
             </form>
 
             <form  @submit.prevent="updateCustomDetails">
               <h4>Update extra details</h4>
               <br>
               <input type="text" v-model="usersDetails.favoriteFood" label="Fav language" placeholder="enter favorite programing language" class="form-control">
-              <v-btn type="submit" depressed small color="primary" @keyup.enter="updateCustomDetails"> update </v-btn>
+              <v-btn type="submit" depressed small color="primary" @keyup.enter="updateCustomDetails" class="update"> update </v-btn>
             </form>
-
-            <div v-if="!linkedGoogle">
-             <h4>Link google account</h4>
-             <v-btn  @click="linkGoogle"><v-icon>mdi-google</v-icon></v-btn>
+              <v-divider class="m-tb-20"></v-divider>
+            <div class="center" v-if="!linkedGoogle">
+              <br>
+             <h4 class="center">Link google account</h4>
+             <br>
+             <v-btn class="center" @click="linkGoogle"><v-icon>mdi-google</v-icon></v-btn>
             </div>
 
-            <div v-if="linkedGoogle">
-             <h4>unlink google account</h4>
-             <v-btn @click="unlinkGoogle"><v-icon color=red>mdi-email-off</v-icon></v-btn>
+            <div class="center" v-if="linkedGoogle">
+             <br>
+             <h4 class="center">unlink google account</h4>
+             <br>
+             <v-btn class="center" @click="unlinkGoogle"><v-icon color=red>mdi-email-off</v-icon></v-btn>
             </div>
 
         </v-card>
